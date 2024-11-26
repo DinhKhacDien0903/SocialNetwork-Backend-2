bc071c2a81643b5d57d7288a155b774ac147714d
diendk <diendk@newwave.vn>
2024-11-26 22:47:25 +0700

    add seed data post and commnent

--- SocialNetworkAPI/SocialNetwork/SocialNetwork.DataAccess/SeedData/SeedData.cs
+++ SocialNetworkAPI/SocialNetwork/SocialNetwork.DataAccess/SeedData/SeedData.cs
@@ -37,6 +37,7 @@ namespace SocialNetwork.DataAccess.SeedData
 
                 await context.SaveChangesAsync();
                 users = await context.Users.Select(x => x).ToListAsync();
+
                 if (!context.Set<RelationshipEntity>().Any())
                 {
                     var relationships = new List<RelationshipEntity>();
@@ -59,6 +60,174 @@ namespace SocialNetwork.DataAccess.SeedData
                         }
                     }
                 }
+
+                //post initial
+                if (!context.Set<PostEntity>().Any())
+                {
+                    var posts = new List<PostEntity>();
+                    var reactionPosts = new List<ReactionPostEntity>();
+
+                    foreach (var user in users)
+                    {
+                        for (int j = 1; j <= 3; j++)
+                        {
+                            var post = new PostEntity
+                            {
+                                PostID = Guid.NewGuid().ToString(),
+                                UserID = user.Id,
+                                Content = $"This is post number {j} by {user.UserName}",
+                                IsDelete = false,
+                                Images = new List<ImagesOfPostEntity>()
+                            };
+
+                            // Thêm hình ảnh cho mỗi bài đăng
+                            for (int k = 1; k <= 2; k++)
+                            {
+                                post.Images.Add(new ImagesOfPostEntity
+                                {
+                                    ImagesOfPostID = Guid.NewGuid().ToString(),
+                                    PostID = post.PostID,
+                                    ImgUrl = $"https://example.com/image{k}_{post.PostID}.jpg",
+                                    IsDeleted = false
+                                });
+                            }
+
+                            posts.Add(post);
+
+                            // Tạo và lưu phản ứng cho bài đăng
+                            foreach (var emotionType in context.Set<EmotionTypeEntity>().ToList())
+                            {
+                                var reaction = new ReactionEntity
+                                {
+                                    ReactionID = Guid.NewGuid().ToString(),
+                                    UserID = user.Id,
+                                    EmotionTypeID = emotionType.EmotionTypeID,
+                                    IsDeleted = false
+                                };
+
+                                await context.Set<ReactionEntity>().AddAsync(reaction);
+                                await context.SaveChangesAsync(); // Lưu lại để ReactionID có trong cơ sở dữ liệu
+
+                                reactionPosts.Add(new ReactionPostEntity
+                                {
+                                    ReactionID = reaction.ReactionID,
+                                    PostID = post.PostID
+                                });
+                            }
+                        }
+                    }
+
+                    // Lưu các bài đăng và liên kết ReactionPostEntity
+                    await context.Set<PostEntity>().AddRangeAsync(posts);
+                    await context.Set<ReactionPostEntity>().AddRangeAsync(reactionPosts);
+                    await context.SaveChangesAsync();
+                }
+                if (!context.Set<PostEntity>().Any())
+                {
+                    var posts = new List<PostEntity>();
+                    var reactionPosts = new List<ReactionPostEntity>();
+
+                    foreach (var user in users)
+                    {
+                        for (int j = 1; j <= 3; j++)
+                        {
+                            var post = new PostEntity
+                            {
+                                PostID = Guid.NewGuid().ToString(),
+                                UserID = user.Id,
+                                Content = $"This is post number {j} by {user.UserName}",
+                                IsDelete = false,
+                                Images = new List<ImagesOfPostEntity>()
+                            };
+
+                            // Thêm hình ảnh cho mỗi bài đăng
+                            for (int k = 1; k <= 2; k++)
+                            {
+                                post.Images.Add(new ImagesOfPostEntity
+                                {
+                                    ImagesOfPostID = Guid.NewGuid().ToString(),
+                                    PostID = post.PostID,
+                                    ImgUrl = $"https://example.com/image{k}_{post.PostID}.jpg",
+                                    IsDeleted = false
+                                });
+                            }
+
+                            posts.Add(post);
+
+                            // Tạo và lưu phản ứng cho bài đăng
+                            foreach (var emotionType in context.Set<EmotionTypeEntity>().ToList())
+                            {
+                                var reaction = new ReactionEntity
+                                {
+                                    ReactionID = Guid.NewGuid().ToString(),
+                                    UserID = user.Id,
+                                    EmotionTypeID = emotionType.EmotionTypeID,
+                                    IsDeleted = false
+                                };
+
+                                await context.Set<ReactionEntity>().AddAsync(reaction);
+                                await context.SaveChangesAsync(); // Lưu lại để ReactionID có trong cơ sở dữ liệu
+
+                                reactionPosts.Add(new ReactionPostEntity
+                                {
+                                    ReactionID = reaction.ReactionID,
+                                    PostID = post.PostID
+                                });
+                            }
+                        }
+                    }
+
+                    // Lưu các bài đăng và liên kết ReactionPostEntity
+                    await context.Set<PostEntity>().AddRangeAsync(posts);
+                    await context.Set<ReactionPostEntity>().AddRangeAsync(reactionPosts);
+                    await context.SaveChangesAsync();
+                }
+
+                // Seed bình luận
+                if (!context.Set<CommentEntity>().Any())
+                {
+                    var comments = new List<CommentEntity>();
+                    var posts = await context.Set<PostEntity>().ToListAsync();
+
+                    foreach (var post in posts)
+                    {
+                        foreach (var user in users)
+                        {
+                            var comment = new CommentEntity
+                            {
+                                CommentID = Guid.NewGuid().ToString(),
+                                UserID = user.Id,
+                                PostID = post.PostID,
+                                Content = $"This is a comment by {user.UserName} on post {post.PostID}",
+                                IsDelete = false,
+                                Children = new List<CommentEntity>()
+                            };
+
+                            comments.Add(comment);
+
+                            // Tạo trả lời cho bình luận chính
+                            for (int i = 1; i <= 2; i++)
+                            {
+                                var reply = new CommentEntity
+                                {
+                                    CommentID = Guid.NewGuid().ToString(),
+                                    UserID = user.Id,
+                                    PostID = post.PostID,
+                                    ParentCommentID = comment.CommentID,
+                                    Content = $"This is reply {i} to comment {comment.CommentID} by {user.UserName}",
+                                    IsDelete = false
+                                };
+
+                                comment.Children.Add(reply);
+                                comments.Add(reply);
+                            }
+                        }
+                    }
+
+                    await context.Set<CommentEntity>().AddRangeAsync(comments);
+                    await context.SaveChangesAsync();
+                }
+
 
                 if (!context.Set<MessagesEntity>().Any())
                 {
