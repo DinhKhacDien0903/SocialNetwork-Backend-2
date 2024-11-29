7554f74c3e52fc0786b902ffe726475a22b37700
dungvt <dungvt@newwave.vn>
2024-11-29 17:00:42 +0900

    display group chat members

--- SocialNetworkAPI/SocialNetwork/SocialNetwork.Web/Controllers/ChatController.cs
+++ SocialNetworkAPI/SocialNetwork/SocialNetwork.Web/Controllers/ChatController.cs
@@ -48,7 +48,7 @@ namespace SocialNetwork.Web.Controllers
 
         [Authorize]
         [HttpGet("getAllConversation")]
-        public async Task<IActionResult> GetAllConversationAsync([FromQuery] SearchConversation search)
+        public async Task<IActionResult> GetAllConversationAsync([FromQuery] BaseSearch search)
         {
             try
             {
@@ -72,7 +72,7 @@ namespace SocialNetwork.Web.Controllers
 
         [Authorize]
         [HttpGet("getFriends")]
-        public async Task<IActionResult> GetFriendsAsync([FromQuery] SearchConversation search)
+        public async Task<IActionResult> GetFriendsAsync([FromQuery] BaseSearch search)
         {
             try
             {
@@ -85,6 +85,55 @@ namespace SocialNetwork.Web.Controllers
                     Status = 200,
                     Message = "Get all conversation success",
                     Data = conversation
+                });
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpGet("getGroupChats")]
+        public async Task<IActionResult> GetGroupChatsAsync([FromQuery] BaseSearch search)
+        {
+            try
+            {
+                var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+                var groupChats = await _conversationService.GetGroupChatsAsync(userId, search);
+
+                return Ok(new BaseResponse
+                {
+                    Status = 200,
+                    Message = "Get all group chats success",
+                    Data = groupChats
+                });
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        [Authorize]
+        [HttpGet("getGroupChatMembers")]
+        public async Task<IActionResult> GetGroupMembersAsync([FromQuery] BaseSearch search)
+        {
+            try
+            {
+                var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+                var groupMembers = await _conversationService.GetGroupMembersAsync(userId, search);
+
+                return Ok(new BaseResponse
+                {
+                    Status = 200,
+                    Message = "Get all group chats success",
+                    Data = groupMembers
                 });
 
             }
